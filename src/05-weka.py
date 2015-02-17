67ea5e242a97570e9ae271c3f0c03d6a4df23a76
Esther Camilo <esthercamilo@gmail.com>
2015-02-17 21:07:49 -0200

    update

--- src/05-weka.py
+++ src/05-weka.py
@@ -5,6 +5,7 @@
 
 import os
 import sys
+import subprocess
 from threading import Thread
 
 finput = open("config.txt")
@@ -19,31 +20,56 @@ l5 = ['cold', 'mix']
 l6 = ['csv']
 
 
-def weka(path):
-	if not os.path.exists(path+"csv/"+file.replace("csv","arff")):
-		command = "java -cp "+wekalocation+" weka.filters.unsupervised.attribute.Remove -R 1,2 -i "+path+ "csv/" + file + " -o "+ path + "arff/" + file.replace('csv','arff')
-		os.system(command)
-	else:
-		print "arff exists"
+def run_cmd_with_system(weka_jar, path, fpath):
+    command = "java -cp "+weka_jar+" weka.filters.unsupervised.attribute.Remove -R 1,2 -i "+path+ "csv/" + fpath + " -o "+ path + "arff/" + fpath.replace('csv','arff')
+    os.system(command)
+
+
+def run_cmd_with_popen(weka_jar, path, fpath):
+    command_arr = [
+        "java -cp",
+        weka_jar,
+        "weka.filters.unsupervised.attribute.Remove",
+        "-R 1,2",
+        "-i", path+'csv/'+fpath,
+        "-o", path+'arff/'+fpath.replace('csv', 'arff')
+    ]
+    proc = subprocess.Popen(
+        command_arr,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    return proc.communicate()
+
+def weka(path, f, weka_jar):
+    trg = os.path.join(path, "csv", f.replace("csv","arff"))
+    if os.path.exists(trg):
+        print("arff exists")
+        return 0
+
+    return run_cmd_with_popen(
+        weka_jar,
+        f,
+        trg
+    )
 
 # Convert csv to arff
-for a in l1:
-	for b in l2:
-		for c in l3:
-			for d in l4:
-				for e in l5:
-					path = (folder+'%s/%s/%s/%s/%s/' % (a, b, c, d, e))
-					lista_files = os.listdir(path + 'csv/')
-					thr = []
-					for file in lista_files:
-						threadweka = Thread(target=weka, args=([path]))
-						thr.append(threadweka)
-					[x.start() for x in thr]
-					[x.join() for x in thr]
-				print d
-			print c
-		print b
+def convert():
+    print '***********CONVERTING FROM CSV TO ARFF - remove gene1 and gene2 attributes\n'
+    results = []
+    for a in l1:
+        for b in l2:
+            for c in l3:
+                for d in l4:
+                    for e in l5:
+                        path = (folder+'%s/%s/%s/%s/%s/' % (a, b, c, d, e))
+                        lista_files = os.listdir(path + 'csv/')
+                        for fpath in lista_files:
+                            res = run_cmd_with_popen(wekalocation, path, fpath)
+                            results.append(res.returncode)
+                        threadweka = Thread(target=weka, args=([fpath]))
+                        thr.append(threadweka)
+    [x.start() for x in thr]
+    [x.join() for x in thr]
 
-
-
-
+convert()
